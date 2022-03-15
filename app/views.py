bf76197cbb1fb5ecdc30f50e7a0a972ec0b900ac
himzies <100895032+himzies@users.noreply.github.com>
2022-03-15 17:05:29 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -92,12 +92,4 @@ def edit(request, id):
     return render(request, "app/edit.html", context)
 
 def home(request):
-    """Shows the main page"""
-    
-    ## Use raw query to get a customer
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM customers WHERE customerid = %s", [id])
-        customer = cursor.fetchone()
-    result_dict = {'cust': customer}
-
     return render(request,'app/home.html',result_dict)
