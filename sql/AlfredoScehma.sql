4985de716fab9519d9b91e020e719613389bf0bf
himzies <100895032+himzies@users.noreply.github.com>
2022-03-15 17:14:19 +0800

    Update and rename AppStoreSchema.sql to AlfredoScehma.sql

---
+++ sql/AlfredoScehma.sql
@@ -0,0 +1,26 @@
+/*******************
+
+  Create the schema
+
+********************/
+
+CREATE TABLE IF NOT EXISTS customers (
+ first_name VARCHAR(64) NOT NULL,
+ last_name VARCHAR(64) NOT NULL,
+ email VARCHAR(64) UNIQUE NOT NULL,
+ dob DATE NOT NULL,
+ since DATE NOT NULL,
+ customerid VARCHAR(16) PRIMARY KEY,
+ country VARCHAR(16) NOT NULL);
+ 
+ CREATE TABLE job_category(
+  PRIMARY KEY name VARCHAR(64));
+ 
+ CREATE TABLE housekeeing(
+  PRIMARY KEY name VARCHAR(64));
+ 
+ CREATE TABLE pet_care(
+  PRIMARY KEY name VARCHAR(64));
+ 
+ CREATE TABLE tuition(
+  PRIMARY KEY name VARCHAR(64));
