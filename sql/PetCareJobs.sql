a97988c06630567d5036d4f2c82c6a2ac8713669
himzies <100895032+himzies@users.noreply.github.com>
2022-03-16 18:03:58 +0800

    Update PetCareJobs.sql

--- sql/PetCareJobs.sql
+++ sql/PetCareJobs.sql
@@ -1,3 +1,3 @@
 INSERT INTO pet_care VALUES ('Pet Grooming', 'Styling of pet fur, bathing services and trimming of nails.');
 INSERT INTO pet_care VALUES ('Walk in the Park', 'We bring your pets for a walk in the park and letting them play with other pets.');
-INSERT INTO pet_care VALUES ('Petsitting', 'We take care of your pet for a certain duration while you\'re busy.');
+INSERT INTO pet_care VALUES ('Petsitting', 'We take care of your pet for a certain duration while you are busy.');
