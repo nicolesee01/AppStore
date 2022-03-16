b9977801e268547d746b33ed22431f0714a5bded
himzies <100895032+himzies@users.noreply.github.com>
2022-03-16 23:55:17 +0800

    Update AlfredoSchema.sql

--- sql/AlfredoSchema.sql
+++ sql/AlfredoSchema.sql
@@ -5,13 +5,12 @@
 ********************/
 
 CREATE TABLE IF NOT EXISTS customers (
+ customerid VARCHAR(16) PRIMARY KEY,
+ password VARCHAR(64) NOT NULL
  first_name VARCHAR(64) NOT NULL,
  last_name VARCHAR(64) NOT NULL,
  email VARCHAR(64) UNIQUE NOT NULL,
- dob DATE NOT NULL,
- since DATE NOT NULL,
- customerid VARCHAR(16) PRIMARY KEY,
- country VARCHAR(16) NOT NULL);
+ address VARCHAR(255) PRIMARY KEY);
  
  CREATE TABLE IF NOT EXISTS job_category(
   name VARCHAR(64) PRIMARY KEY);
